32195ca5b4c3a88acea51d137959ca1a9108c53c
Sélim Ollivier <selim.ollivier@onera.fr>
2023-09-27 16:37:49 +0200

    configure package

---
+++ setup.py
@@ -0,0 +1,25 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+VERSION = '0.1.0'
+PACKAGE_NAME =  'toolbox'
+AUTHOR = 'Sélim Ollivier'
+AUTHOR_EMAIL = 'selim.ollivier@onera.fr'
+LICENSE = 'MIT license'
+DESCRIPTION = 'Usefull tools for DL/CV projects using PyTorch'
+LONG_DESCRIPTION = (HERE / "README.md").read_text()
+LONG_DESC_TYPE = "text/markdown"
+INSTALL_REQUIRES = [
+    'torch'
+]
+
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    license=LICENSE,
+    install_requires=INSTALL_REQUIRES,
+    packages=find_packages()
+)
